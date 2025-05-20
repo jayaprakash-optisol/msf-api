118954213b026c99b5134ba7bf4bea2207b82f99
jayaprakash-optisol <jayaprakash.j@optisolbusiness.com>
2025-05-20 11:27:24 +0530

    Update enums.ts

--- src/models/enums.ts
+++ src/models/enums.ts
@@ -4,7 +4,7 @@ import { pgEnum } from 'drizzle-orm/pg-core';
 export const roleEnum = pgEnum('role', ['Admin', 'User']);
 
 // Guest Role Enum
-export const guestRoleEnum = pgEnum('guest_role', ['Stock Manager', 'Store Keeper']);
+export const guestRoleEnum = pgEnum('guest_role', ['Stock Manager', 'Store Keeper', 'Guest User']);
 
 // Guest Status Enum
 export const guestStatusEnum = pgEnum('guest_status', ['Active', 'Inactive', 'Expired']);
